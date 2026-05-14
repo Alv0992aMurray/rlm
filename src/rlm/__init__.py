40a0987efc66544b4ef1f701da19d3801d38c1f6
Alvaro Murray <Alv0992aMurray@users.noreply.github.com>
2026-05-14 22:59:18 +0800

    Add note referencing upstream repo in module docstring

--- src/rlm/__init__.py
+++ src/rlm/__init__.py
@@ -2,6 +2,9 @@
 
 A library for training and evaluating language models using
 reinforcement learning techniques.
+
+Note: Forked from alexzhang13/rlm for personal experimentation.
+See https://github.com/alexzhang13/rlm for the original project.
 """
 
 __version__ = "0.1.0"
